3594544eb4b03874fc09fa6c6ccc03ea86183803
AriettaaS <93552006+AriettaaS@users.noreply.github.com>
2022-12-28 19:27:13 -0500

    Se hace la funcion doLogin()

--- js/myScript.js
+++ js/myScript.js
@@ -13,4 +13,30 @@ function showLogin(){
               <button onclick ="doLogin()">Ingresar</button> 
               <p id="mensaje" ></p>`;
   document.getElementById('main').innerHTML = formhtml;
+
+}
+
+/**
+ * Esta función recolecta los valores ingresados en el formulario
+ * y los envía al CGI login.pl
+ * La respuesta del CGI es procesada por la función loginResponse
+ */
+function doLogin(){
+  let user = document.getElementById('user').value;
+  let password = document.getElementById('password').value;
+  console.log(user);
+  console.log(password);
+  let url = 'cgi-bin/login.pl?user='+user+'&password='+password;
+  console.log(url);
+  var xml;
+  let promise = fetch(url);
+  promise.then(response=>response.text()).then(data=>
+    {
+      xml = (new window.DOMParser()).parseFromString(data, "text/xml");
+      console.log(xml);
+      loginResponse(xml) ;
+    }).catch(error=>{
+      console.log('Error :', error);
+    });
+
 }
